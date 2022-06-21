6cd5c50a9432e2240fc4ee82c8c697ef14f5f725
febryfairuz <febryfairuz@gmail.com>
2022-06-21 14:19:42 +0700

    master-data

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pem-Web-2021-2022
+Kelas Reguler TI-20-PA dan Kelas Karyawan TI-20-KA
---
+++ assets/js/const/CoursesData.js
@@ -0,0 +1,62 @@
+const CoursesData = [
+  {
+    id: 1,
+    code: "ABC-123",
+    title: "Pemograman Perangkat Bergerak",
+    startDay: "Jumat",
+    startTime: "07.30 - 08.30",
+    icon: "phone-portrait-outline",
+    semester: 4,
+    credit: 3,
+    room:"201",
+    lecturer:{
+      id:3320190306,
+      name:"Febri Damatraseta F S.T, M.Kom"
+    }
+  },
+  {
+    id: 2,
+    code: "ABC-321",
+    title: "Lab Pemograman Perangkat Bergerak",
+    startDay: "Sabtu",
+    startTime: "08.30 - 09.30",
+    icon: "phone-portrait-outline",
+    semester: 4,
+    credit: 0,
+    room:"201",
+    lecturer:{
+      id:3320190306,
+      name:"Febri Damatraseta F S.T, M.Kom"
+    }
+  },
+  {
+    id: 3,
+    code: "ABC-456",
+    title: "Pemograman Website",
+    startDay: "Jumat",
+    startTime: "10.00 - 11.00",
+    icon: "globe-outline",
+    semester: 4,
+    credit: 3,
+    room:"215",
+    lecturer:{
+      id:3320190306,
+      name:"Febri Damatraseta F S.T, M.Kom"
+    }
+  },
+  {
+    id: 4,
+    code: "ABC-654",
+    title: "Lab Pemograman Website",
+    startDay: "Sabtu",
+    startTime: "11.00 - 13.00",
+    icon: "globe-outline",
+    semester: 4,
+    credit: 0,
+    room:"215",
+    lecturer:{
+      id:3320190306,
+      name:"Febri Damatraseta F S.T, M.Kom"
+    }
+  },
+];
